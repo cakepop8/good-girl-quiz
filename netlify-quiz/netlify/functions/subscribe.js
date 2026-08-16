f033215034dd3e0c5b960107535ff08720bb4a89
cakepop8 <caitlin@caitlinjudd.com>
2026-08-16 17:39:57 +1000

    Refactor subscription API call to use environment variables

--- netlify-quiz/netlify/functions/subscribe.js
+++ netlify-quiz/netlify/functions/subscribe.js
@@ -11,14 +11,13 @@ exports.handler = async function(event, context) {
 
   try {
     const { email, first_name, archetype } = JSON.parse(event.body);
-
     const response = await fetch(
-      'https://api.beehiiv.com/v2/publications/pub_569dd26f-3789-412d-950f-e5179aa49e78/subscriptions',
+      `https://api.beehiiv.com/v2/publications/${process.env.BEEHIIV_PUB_ID}/subscriptions`,
       {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': 'Bearer 9JHx3UScDnAOv5pXyfPiPtXIXsTtJOrn0ItSxIUJkRUU53aDQnK49DmDy80rm6Vp'
+          'Authorization': `Bearer ${process.env.BEEHIIV_API_KEY}`
         },
         body: JSON.stringify({
           email,
@@ -29,7 +28,6 @@ exports.handler = async function(event, context) {
         })
       }
     );
-
     const data = await response.json();
     return { statusCode: 200, headers, body: JSON.stringify({ success: true, data }) };
   } catch (err) {
