4db38675cc7c755d12f6231266dcbfe5cc7c8a9f
cakepop8 <caitlin@caitlinjudd.com>
2026-08-19 20:13:24 +1000

    Add automation IDs for subscription archetypes
    Added automation IDs mapping for different archetypes and included automation ID in the subscription request.

--- netlify-quiz/netlify/functions/subscribe.js
+++ netlify-quiz/netlify/functions/subscribe.js
@@ -1,3 +1,18 @@
+const AUTOMATION_IDS = {
+  'THE COOL GIRL': 'c4cdb636-bf29-4fde-82c8-6a369f6c87f3',
+  'THE FORTRESS': '27efd4d1-8e22-4e3e-944d-14603c818474',
+  'THE GOLD STAR GIRL': '11aca65b-bd88-4a26-b689-e6b5d00b284b',
+  'THE HIDDEN ARTIST': '1e4f287c-a49c-4dfc-af58-ef0de29cbe87',
+  'THE LADY-IN-WAITING': '73f14b1c-fa9d-4e14-b682-253ff0253cf3',
+  'THE LOLLIPOP LADY': 'ae1f51b6-0a0e-4ce7-be03-3e17f0a8ba91',
+  'THE MILESTONE MAKER': '177a0df1-3e29-45a4-af51-58aa5ffa5246',
+  'THE PERFECTIONISTA': '636ccf90-8b5a-405f-9592-517a7a195229',
+  'THE RUMINATOR': 'eda7f0ac-bb17-45f5-a488-c454e8c60d41',
+  'THE SAINT': '4a9668f1-89da-4a92-892b-e320071c953a',
+  'THE SHAPESHIFTER': '6562b8c0-48bb-4a3b-acbc-ea7ec86276a4',
+  'THE WELLNESS GURU': '420ae4fc-2054-4a28-9249-f7605a0c8752'
+};
+
 exports.handler = async function(event, context) {
   const headers = {
     'Access-Control-Allow-Origin': '*',
@@ -11,6 +26,9 @@ exports.handler = async function(event, context) {
 
   try {
     const { email, first_name, archetype } = JSON.parse(event.body);
+    const automationId = AUTOMATION_IDS[archetype];
+
+    console.log('Subscribing:', email, first_name, archetype, automationId);
 
     const url = `https://api.beehiiv.com/v2/publications/${process.env.BEEHIIV_PUB_ID}/subscriptions`;
 
@@ -25,8 +43,12 @@ exports.handler = async function(event, context) {
         first_name,
         reactivate_existing: true,
         send_welcome_email: false,
+        automation_ids: automationId ? [automationId] : [],
         tags: ['good girl quiz'],
-        custom_fields: [{ name: 'Good Girl Archetype', value: archetype }]
+        custom_fields: [
+          { name: 'Good Girl Archetype', value: archetype },
+          { name: 'First Name', value: first_name }
+        ]
       })
     });
 
